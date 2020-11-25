ddd07d6bf19ea610153c98f42fa2c73f57192110
jyan83 <yankaron@gmail.com>
2020-11-24 22:36:27 -0600

    validation extract

--- explore/TIRTLE_extract.py
+++ explore/TIRTLE_extract.py
@@ -1,35 +1,44 @@
 from datetime import timedelta, datetime
 from pathlib import Path
 import matplotlib.pyplot as plt
+import matplotlib.dates as md
 import numpy as np
 import pandas as pd
 
 def load_data(data_files, timestamp_start=None, timestamp_end=None):
-    df = pd.read_csv(data_files, sep=",", header=0, index_col=0, low_memory=False)
+    df = pd.read_csv(data_files, sep=",", header=0, parse_dates={'datetime':[0,1]}, low_memory=False)
+    df.index = pd.to_datetime(df['datetime'], errors='coerce', format='%Y-%m-%d %H:%M:%S')
     if timestamp_start is not None:
-        df = df[(df.index.get_level_values(0) >= timestamp_start) & (df.index.get_level_values(0) <= timestamp_end)]    
+        df = df[(df.index >= timestamp_start) & (df.index <= timestamp_end)]   
     return df
 
 #%% Visualization
 def plot_events(lane_data, timestamp_start=None, timestamp_end=None, title_name=None):
     fig, ax = plt.subplots()
-    plt.scatter(lane_data.time, lane_data["speed(kph)"])
-    plt.vlines(lane_data.time, 0, lane_data["speed(kph)"])
+    plt.scatter(lane_data.index, lane_data["speed(kph)"])
+    plt.vlines(lane_data.index, 0, lane_data["speed(kph)"])
     plt.ylabel('Speed (KPH)')
     plt.title(title_name)
     if timestamp_start is not None:
         plt.xlim([timestamp_start, timestamp_end])
-    my_xticks = ax.get_xticks()
-    plt.xticks([my_xticks[0], my_xticks[-1]], visible=True, rotation=00)
+    xfmt = md.DateFormatter('%H:%M:%S')
+    ax.xaxis.set_major_formatter(xfmt)
     plt.show()
 
 
 if __name__ == '__main__':
     # note: change below path
-    data_files = r'C:\Users\Jin Yan\OneDrive - PARC\FiBridge\Phase_3\DriveEasy\M80\TIRTLE validation\20201124\TIRTLE\T0490_vehicle_20201125_070000_+1100_1h.csv'
+    fbgs_data = np.load(r'C:\Users\Jin Yan\OneDrive - PARC\FiBridge\Phase_3\DriveEasy\M80\TIRTLE validation\20201124\driveeasy_wav\wav_20201124_195912_F01_UTC.npz', allow_pickle=True)
+    fbgs = pd.DataFrame(data=fbgs_data['wav'], 
+                      columns=[f'sensor{i + 1}' for i in range(25)], 
+                      index=fbgs_data['timestamp'])
+    fbgs_timestamp = fbgs.index
+    
     # select start & end timestamp
-    # timestamp_start = '2020/11/20 00:01:13.000'
-    # timestamp_end = '2020/11/20 00:12:18'
+    timestamp_start = fbgs_timestamp[0] + timedelta(hours=10)
+    timestamp_end = fbgs_timestamp[-1] + timedelta(hours=10)
+    
+    data_files = r'C:\Users\Jin Yan\OneDrive - PARC\FiBridge\Phase_3\DriveEasy\M80\TIRTLE validation\20201124\TIRTLE\T0490_vehicle_20201125_070000_+1100_1h.csv'
     
     # load data and saparate lanes
     title = load_data(data_files)    
